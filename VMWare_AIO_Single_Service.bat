2d77fef238bfc8dec633a3c65f4fa2196cbb48e1
Konen <angshumanchakrabartty@gmail.com>
2022-02-11 22:24:07 +0530

    Adding Aio Tool to the Repo
    Adding Aio File which contains features from the both files

---
+++ VMWare_AIO_Single_Service.bat
@@ -0,0 +1,35 @@
+@echo off
+	  sc config "VMware Workstation Server" start= demand
+	  sc config "VMware NAT Service" start= demand
+	  sc config "VMUSBArbService" start= demand
+	  sc config "VMnetDHCP" start= demand
+	  sc config "VMAuthdService" start= demand
+    echo Start/Stop VMware Workstation services
+    echo.
+    echo 1. Start
+    echo 2. Stop
+    echo.
+    choice /c:12
+
+    If errorlevel 1 if not errorlevel 2 goto start
+    If errorlevel 2 if not errorlevel 3 goto stop
+
+    :start
+    echo.
+    net start "VMware Workstation Server"
+    net start "VMware Authorization Service"
+    net start "VMware NAT Service"
+    net start "VMware DHCP Service"
+    net start "VMware USB Arbitration Service"
+    goto end
+
+    :stop
+    echo.
+    net stop "VMware Workstation Server"
+    net stop "VMware Authorization Service"
+    net stop "VMware NAT Service"
+    net stop "VMware DHCP Service"
+    net stop "VMware USB Arbitration Service"
+    goto end
+
+    :end
